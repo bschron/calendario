5b5dc760a4189d91d3c4fa4ff2b11604abb24704
bschron <brunochroniaris@outlook.com>
2014-11-05 15:49:44 -0300

    created project

---
+++ calendar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calendar
+//
+//  Created by Bruno Chroniaris on 11/5/14.
+//  Copyright (c) 2014 UNIUM automação. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
